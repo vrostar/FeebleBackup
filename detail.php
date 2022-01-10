d6564e67a5b4bd3d916fca2b90ab1ed0aa10be4e
vrostar <89836816+vrostar@users.noreply.github.com>
2022-01-10 01:27:52 +0100

    Update detail.php

--- detail.php
+++ detail.php
@@ -36,7 +36,7 @@ mysqli_close($db);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Details - <?= $user['name'] ?></title>
-    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
 <header>
